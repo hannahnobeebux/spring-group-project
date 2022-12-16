ee654d8a54b4d2a672837c3254e4d94188d37a02
vicbrooke <113979837+vicbrooke@users.noreply.github.com>
2022-12-16 16:27:42 +0000

    users page added

---
+++ src/components/Users.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import fetchGetAllUsers from "../utils/Users/fetchGetAllUsers";
+
+export default function Users() {
+  const [users, setUsers] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      const data = await fetchGetAllUsers();
+      setUsers(data);
+      setIsLoading(false);
+    }
+    fetchUsers();
+  }, []);
+
+  if (!isLoading) {
+    console.log(users);
+    return users.map((user) => (
+      <section key={user.id}>
+        <h3>{user.firstName}</h3>
+        <h3>{user.lastName}</h3>
+      </section>
+    ));
+  }
+}
