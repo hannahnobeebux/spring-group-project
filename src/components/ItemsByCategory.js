d4b8b3cfc8e14a36b758d620ea8a5f9da805acbd
vicbrooke <113979837+vicbrooke@users.noreply.github.com>
2022-12-16 12:23:42 +0000

    Items by category page added

---
+++ src/components/ItemsByCategory.js
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import styled from "styled-components";
+import fetchGetAllItemsByCategory from "../utils/Items/fetchGetAllItemsByCategory";
+
+export default function ItemsByCategory() {
+  const [items, setItems] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const { category } = useParams();
+  console.log(category);
+
+  useEffect(() => {
+    async function fetchItemsByCategory() {
+      const data = await fetchGetAllItemsByCategory(category);
+      console.log(data);
+      setItems(data);
+      setIsLoading(false);
+    }
+    fetchItemsByCategory();
+  }, [category]);
+
+  if (!isLoading) {
+    console.log(items);
+    return items.map((item) => (
+      <Section key={item.id}>
+        <Link to={`/itemInfo/${item.id}`}>
+          <SubHeading>
+            {item.name.charAt(0).toUpperCase() + item.name.slice(1)}
+          </SubHeading>
+          <PTag>£{item.price.toFixed(2)}</PTag>
+          <Image src={item.image} />
+        </Link>
+        <Wishlist>Add to wishlist</Wishlist>
+      </Section>
+    ));
+  }
+}
+
+// Styling
+
+const Section = styled.section`
+  background-color: orange;
+  width: 20vw;
+  height: 25vw;
+  margin: 2vw 1vw;
+  display: flex;
+  flex-direction: column;
+`;
+
+const SubHeading = styled.h3`
+  margin: 10px;
+`;
+
+const PTag = styled.p`
+  margin-top: 0;
+`;
+
+const Image = styled.img`
+  width: 15vw;
+  height: 15vw;
+  align-self: center;
+`;
+
+const Wishlist = styled.button`
+  margin-top: 1vw;
+  width: fit-content;
+  align-self: center;
+  justify-self: flex-end;
+`;
--- src/utils/Items/fetchGetAllitemsByCategory.js
+++ src/utils/Items/fetchGetAllitemsByCategory.js
@@ -1,17 +1,19 @@
 export default async function fetchGetAllItemsByCategory(itemId) {
-    try {
-      const response = await fetch(`http://localhost:8080/shop/item/category/${itemId}`);
-      const data = await response.json();
-      if (response.status === 200) {
-      } else {
-        if (data.errors !== undefined) {
-          alert(data.errors[0].msg);
-          return;
-        }
-        alert(data.message);
+  try {
+    const response = await fetch(
+      `http://localhost:8080/shop/item/category/${itemId}`
+    );
+    const data = await response.json();
+    if (response.status === 200) {
+      return data;
+    } else {
+      if (data.errors !== undefined) {
+        alert(data.errors[0].msg);
+        return;
       }
-    } catch (error) {
-      alert(error.message);
+      alert(data.message);
     }
+  } catch (error) {
+    alert(error.message);
   }
-  
+}
