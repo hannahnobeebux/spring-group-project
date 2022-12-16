7a3230bd4ebcac3486c660737f34116c6781b058
vicbrooke <113979837+vicbrooke@users.noreply.github.com>
2022-12-16 16:29:40 +0000

    styling updated

--- src/components/ItemInfo.js
+++ src/components/ItemInfo.js
@@ -20,7 +20,7 @@ export default function ItemInfo() {
 
   if (!isLoading) {
     return (
-      <section>
+      <ItemSection>
         <SubHeading>
           {item.name.charAt(0).toUpperCase() + item.name.slice(1)}
         </SubHeading>
@@ -35,23 +35,40 @@ export default function ItemInfo() {
           Description: <br></br>
           {item.description}
         </PTag>
-      </section>
+      </ItemSection>
     );
   }
 }
 
 // Styling
 
+const ItemSection = styled.section`
+  display: grid;
+  grid-template-rows: 0.5fr 0.1fr 0.1fr 0.1fr 0.1fr;
+  grid-template-columns: 1fr 1fr;
+`;
+
 const SubHeading = styled.h3`
-  margin: 10px;
+  grid-column-start: 0;
+  grid-column-end: 1;
+  margin: 20px;
+  font-size: 40px;
 `;
 
 const PTag = styled.p`
-  margin-top: 0;
+  margin-top: 60px;
+  margin-right: 400px;
+  grid-column-start: 3;
+  grid-column-end: 4;
 `;
 
 const Image = styled.img`
-  width: 15vw;
-  height: 15vw;
-  align-self: center;
+  grid-row-start: 2;
+  grid-row-end: 400;
+  grid-column-start: 0;
+  grid-column-end: 1;
+  width: 30vw;
+  height: 30vw;
+  margin-left: 2vw;
+  align-self: left;
 `;
