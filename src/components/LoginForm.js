4e4d083da08641000cc7c110e103a70d20948fc3
vicbrooke <113979837+vicbrooke@users.noreply.github.com>
2022-12-19 17:03:38 +0000

    login page created

---
+++ src/components/LoginForm.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+export default function LoginForm() {
+  // Create form
+  // Make post request to auth server with email and password
+  // Get token from server
+  // Save token in local storage
+  //
+  const navigate = useNavigate();
+  const [usernameInput, setUsernameInput] = useState("");
+  const [passwordInput, setPasswordInput] = useState("");
+
+  async function onFormSubmit(e) {
+    e.preventDefault();
+    // Send data
+
+    try {
+      const response = await fetch("http://localhost:8080/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+          emailAddress: `${usernameInput}`,
+          password: `${passwordInput}`,
+        }),
+      });
+      if (response.status === 200) {
+        const { accessToken, tokenType } = await response.json();
+        console.log(accessToken);
+        console.log(tokenType);
+        localStorage.setItem("access_token", accessToken);
+        localStorage.setItem("token_type", tokenType);
+        navigate("/loggedIn");
+        return;
+      }
+    } catch (error) {
+      console.log(error);
+      alert(error.message);
+    }
+  }
+  return (
+    <div>
+      <h1>Login Form!</h1>
+      <form onSubmit={onFormSubmit}>
+        <p>Username</p>
+        <input
+          value={usernameInput}
+          onChange={(e) => setUsernameInput(e.target.value)}
+        ></input>
+        <p>Password</p>
+        <input
+          type="password"
+          value={passwordInput}
+          onChange={(e) => setPasswordInput(e.target.value)}
+        ></input>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
