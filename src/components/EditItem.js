eb9145bc8c5aedf4a9b3eb22d2e8f3f253cbda96
vicbrooke <113979837+vicbrooke@users.noreply.github.com>
2022-12-19 16:27:23 +0000

    Edit item refactored so now updates item when only passed some fields, also automatically receives id and category

--- src/components/EditItem.js
+++ src/components/EditItem.js
@@ -1,4 +1,5 @@
 import { useForm } from "react-hook-form";
+import { useLocation, useNavigate, useParams } from "react-router";
 import styled from "styled-components";
 import fetchEditOneItem from "../utils/Items/fetchEditOneItem";
 
@@ -12,17 +13,27 @@ export default function EditItem() {
       name: "",
     },
   });
+
+  // id will be set to the value of the :id param
+  const { id } = useParams();
+
+  // retrieves param information from url
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const param1 = searchParams.get("param1");
+
+  const navigate = useNavigate();
+
   async function onSubmit(data) {
-    console.log(data);
     await fetchEditOneItem(data);
+    navigate(`/itemInfo/${id}`);
   }
-  console.log(errors);
 
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
         <h2>Edit your item here:</h2>
-        <Input1 {...register("itemId")} placeholder="Item Id" />
+        <Input1 {...register("itemId")} readOnly value={id} />
         <p>{errors.itemId?.message}</p>
         <Input {...register("name")} placeholder="Title" />
         <p>{errors.name?.message}</p>
@@ -34,12 +45,12 @@ export default function EditItem() {
         <br></br>
         <Select
           {...register("category", { required: "This is required" })}
-          placeholder="Category"
+          defaultValue={param1}
         >
           <option value="Baby">Baby</option>
           <option value="Books">Books</option>
-          <option value="Entertainment">Entertainment</option>
           <option value="Fashion">Fashion</option>
+          <option value="Garden">Entertainment</option>
           <option value="Home">Home</option>
           <option value="Toys">Toys</option>
           <option value="Technology">Technology</option>
