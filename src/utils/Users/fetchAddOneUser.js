6ef26d2dfea17fce4a554d964b3427e1af152a20
vicbrooke <113979837+vicbrooke@users.noreply.github.com>
2022-12-19 12:39:59 +0000

    Updated

--- src/utils/Users/fetchAddOneUser.js
+++ src/utils/Users/fetchAddOneUser.js
@@ -1,26 +1,25 @@
-export default async function fetchAddOneUser() {
-    try {
-      const response = await fetch(`http://localhost:8080/shop/user`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json"},
-        body: JSON.stringify({
-            firstName: firstname,
-            lastName: lastname,
-            password: password,
-            emailAddress: emailAddress,
-        })
-      });
-      const data = await response.json();
-      if (response.status === 200) {
-      } else {
-        if (data.errors !== undefined) {
-          alert(data.errors[0].msg);
-          return;
-        }
-        alert(data.message);
+export default async function fetchAddOneUser(user) {
+  try {
+    const response = await fetch(`http://localhost:8080/shop/user`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        firstName: user.firstname,
+        lastName: user.lastname,
+        password: user.password,
+        emailAddress: user.emailAddress,
+      }),
+    });
+    const data = await response.json();
+    if (response.status === 200) {
+    } else {
+      if (data.errors !== undefined) {
+        alert(data.errors[0].msg);
+        return;
       }
-    } catch (error) {
-      alert(error.message);
+      alert(data.message);
     }
+  } catch (error) {
+    alert(error.message);
   }
-  
+}
--- src/utils/Users/fetchEditOneUser.js
+++ src/utils/Users/fetchEditOneUser.js
@@ -1,26 +1,28 @@
-export default async function fetchEditOneUser(userId) {
-    try {
-      const response = await fetch(`http://localhost:8080/shop/User${userId}`, {
+export default async function fetchEditOneUser(user) {
+  try {
+    const response = await fetch(
+      `http://localhost:8080/shop/User${user.userId}`,
+      {
         method: "PUT",
-        headers: { "Content-Type": "application/json"},
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-            firstName: firstname,
-            lastName: lastname,
-            password: password,
-            emailAddress: emailAddress,
-        })
-      });
-      const data = await response.json();
-      if (response.status === 200) {
-      } else {
-        if (data.errors !== undefined) {
-          alert(data.errors[0].msg);
-          return;
-        }
-        alert(data.message);
+          firstName: user.firstname,
+          lastName: user.lastname,
+          password: user.password,
+          emailAddress: user.emailAddress,
+        }),
       }
-    } catch (error) {
-      alert(error.message);
+    );
+    const data = await response.json();
+    if (response.status === 200) {
+    } else {
+      if (data.errors !== undefined) {
+        alert(data.errors[0].msg);
+        return;
+      }
+      alert(data.message);
     }
+  } catch (error) {
+    alert(error.message);
   }
-  
+}
