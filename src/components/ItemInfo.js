2a52dc8f68c4452d0bce273650340c8048aceaa8
vicbrooke <113979837+vicbrooke@users.noreply.github.com>
2022-12-19 17:06:20 +0000

    styling updated

--- src/components/ItemInfo.js
+++ src/components/ItemInfo.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import styled from "styled-components";
 import fetchGetOneItem from "../utils/Items/fetchGetOneItem";
 
@@ -24,17 +24,29 @@ export default function ItemInfo() {
         <SubHeading>
           {item.name.charAt(0).toUpperCase() + item.name.slice(1)}
         </SubHeading>
-        <PTag>
-          Category:{" "}
-          {item.category.charAt(0).toUpperCase() + item.category.slice(1)}
-        </PTag>
         <Image src={item.image} />
-        <PTag>£{item.price}</PTag>
-        <PTag>Quantity: {item.quantity}</PTag>
-        <PTag>
-          Description: <br></br>
-          {item.description}
-        </PTag>
+        <InfoSection>
+          <SubTitleTag>
+            Category:{" "}
+            {item.category.charAt(0).toUpperCase() + item.category.slice(1)}
+          </SubTitleTag>
+
+          <PTag>£{item.price}</PTag>
+          <PTag>Quantity: {item.quantity}</PTag>
+          <PTag>
+            Description: <br></br>
+            {item.description}
+          </PTag>
+          <Link
+            id="edit-button"
+            to={{
+              pathname: `/editItem/${id}`,
+              search: `?param1=${item.category}`,
+            }}
+          >
+            <EditButton>Edit item</EditButton>
+          </Link>
+        </InfoSection>
       </ItemSection>
     );
   }
@@ -42,24 +54,51 @@ export default function ItemInfo() {
 
 // Styling
 
+const InfoSection = styled.section`
+  background-color: #3f8b92;
+  grid-column-start: 1;
+  grid-column-end: 3;
+  grid-row-start: 2;
+  /* grid-row-end: 400; */
+  margin-left: 2vw;
+  text-align: center;
+  align-items: center;
+  padding-left: 400px;
+  width: auto;
+  padding-bottom: 1vw;
+  padding-top: 0;
+`;
+
 const ItemSection = styled.section`
+  /* display: flex; */
   display: grid;
   grid-template-rows: 0.5fr 0.1fr 0.1fr 0.1fr 0.1fr;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr;
 `;
 
 const SubHeading = styled.h3`
   grid-column-start: 0;
   grid-column-end: 1;
-  margin: 20px;
+  /* margin: 20px; */
   font-size: 40px;
+  text-align: center;
 `;
 
 const PTag = styled.p`
   margin-top: 60px;
   margin-right: 400px;
-  grid-column-start: 3;
+  grid-column-start: 1;
   grid-column-end: 4;
+  /* overflow-wrap: break-word; */
+`;
+
+const SubTitleTag = styled.p`
+  margin-top: 60px;
+  margin-right: 400px;
+  margin-bottom: 10px;
+  grid-column-start: 1;
+  grid-column-end: 4;
+  font-weight: bold;
 `;
 
 const Image = styled.img`
@@ -67,8 +106,38 @@ const Image = styled.img`
   grid-row-end: 400;
   grid-column-start: 0;
   grid-column-end: 1;
-  width: 30vw;
-  height: 30vw;
+  width: 20vw;
+  height: 20vw;
   margin-left: 2vw;
   align-self: left;
+`;
+
+const EditButton = styled.button`
+  /* padding: 10px; */
+  /* grid-row-start: 6;
+  grid-row-end: 400;
+  grid-column-start: 1;
+  grid-column-end: 1; */
+  /* grid-column: 1;
+  grid-row-start: 6;
+  grid-row-end: 400; */
+  display: flex;
+  margin-left: 1vw;
+  text-align: center;
+  border-color: orange;
+
+  align-content: center;
+  border-style: none;
+  width: 8vw;
+  margin-bottom: 1vw;
+  padding: 1vw;
+
+  align-self: center;
+  color: White;
+  border-radius: 20px;
+  background-color: #ffa372;
+
+  &:hover {
+    background-color: #e07426;
+  }
 `;
